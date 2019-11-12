03c731e9052d5f62cbc2f0940ae69848fc84dabd
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-12 23:31:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,8 @@ def badge_maker(speaker)
 end
 
 def batch_badge_creator(attendees)
+  badges = []
   attendees.each do |attendee|
-    "Hello, my name is #{attendee}."
+    badges << "Hello, my name is #{attendee}."
   end
 end
